dc7e2064070c750a5cbabcd076e716aa6a16476d
m-kid <maciej.kidawa2@gmail.com>
2016-10-22 10:56:27 +0200

    usunięta zbędna zmienna

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,13 @@ var coeducation = mensNames.concat(womensNames);
 
 console.log(coeducation)
 
-var newName = 'Johnson'
+var newName = 'Henry'
 var indexOfCoeducation = coeducation.indexOf(newName);
 
 console.log(indexOfCoeducation);
 
 if (indexOfCoeducation < 0) {
-    var x = coeducation.push(newName);
+    coeducation.push(newName);
     console.log('Gratulacje! Imienia "', newName, '", które dodałeś, nie było do tej pory na naszej liście. Dopisujemy je na koniec!')
 } else {
     console.log('Niestety, imię, które wpisałeś, czyli "',newName,'", znajduje się już na naszej liście (zajmuje miejsce ', indexOfCoeducation + 1, '). Lista poniżej nie powinna się różnić od listy powyżej')
